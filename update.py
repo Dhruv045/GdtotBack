e430acc58ade2f4e61fda2f6035f6573621a860d
Dhruv045 <106961058+Dhruv045@users.noreply.github.com>
2022-08-20 13:15:25 +0530

    Update Upstream Repo

--- update.py
+++ update.py
@@ -48,8 +48,8 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email e.anastayyar@gmail.com \
-                 && git config --global user.name mltb \
+                 && git config --global user.email toonsindia001@gmail.com \
+                 && git config --global user.name dhruv045 \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
